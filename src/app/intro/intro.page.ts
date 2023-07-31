3801e744320b78e1385b220acba672408d20ba8b
dtarnawsky <damian@ionic.io>
2023-07-31 12:26:43 -0700

    feat: status bar color improve

--- src/app/intro/intro.page.ts
+++ src/app/intro/intro.page.ts
@@ -12,6 +12,7 @@ import { addDays, daysBetween, now } from '../utils';
 import { Dataset } from '../models';
 import { datasetFilename } from '../api';
 import { ApiService } from '../api.service';
+import { StatusBar, Style } from '@capacitor/status-bar';
 
 @Component({
   selector: 'app-intro',
@@ -33,6 +34,7 @@ export class IntroPage implements OnInit {
     private fav: FavoritesService, private router: Router) { }
 
   async ngOnInit() {
+    StatusBar.setStyle({style: Style.Dark});
     this.cards = await this.loadDatasets();
     this.selected = this.cards[0];
     this.save(); // Needed in case user has restarted
@@ -79,6 +81,7 @@ export class IntroPage implements OnInit {
       this.fav.init(this.settingsService.settings.dataset);
       const title = (this.selected.year == this.cards[0].year) ? '' : this.selected.year;
       this.db.selectedYear.set(title);
+      StatusBar.setStyle({style: Style.Default });
       this.router.navigateByUrl('/tabs/events');
     } finally {
       this.ready = true;
