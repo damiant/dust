b746f4fff4866b59d3fcb6b1b7c7c9fa0cf86085
damian.ionic <damian@ionic.io>
2023-08-23 09:08:08 -0700

    fix: add center camp to streets

--- src/app/map/street.service.ts
+++ src/app/map/street.service.ts
@@ -7,7 +7,7 @@ import { PickerColumnItem } from '@ionic/core';
 })
 export class StreetService {
 
-  private allStreets = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'Esplanade'];
+  private allStreets = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'Esplanade', 'Center Camp'];
   private allHours = ['2', '3', '4', '5', '6', '7', '8', '9', '10'];
   private allMinutes = ['00', '05', '10', '15', '20', '25', '30', '35', '40', '45', '50', '55'];
 
