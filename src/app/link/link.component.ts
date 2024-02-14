d827ebcf5ffb6d3fdaf351f078b8032ee011d6cd
Damian <damiantarnawsky@gmail.com>
2024-02-13 16:09:05 -0800

    2.0.4 (#61)
    * feat: allow images as links
    
    * fix: loading images
    
    * feat: send to slack
    
    ---------
    
    Co-authored-by: damian.ionic <damian@ionic.io>

--- src/app/link/link.component.ts
+++ src/app/link/link.component.ts
@@ -1,15 +1,16 @@
 import { Component, computed, input } from '@angular/core';
 import { UiService } from '../ui/ui.service';
-import { IonItem, IonIcon } from "@ionic/angular/standalone";
+import { IonItem, IonIcon, IonCard, IonCardContent } from "@ionic/angular/standalone";
 import { addIcons } from 'ionicons';
 import { compass, compassOutline, linkOutline, callOutline, mailOutline, ticketOutline, tvOutline } from 'ionicons/icons';
 import { CommonModule } from '@angular/common';
+import { CachedImgComponent } from '../cached-img/cached-img.component';
 @Component({
   selector: 'app-link',
   templateUrl: './link.component.html',
   styleUrls: ['./link.component.scss'],
   standalone: true,
-  imports: [IonIcon, IonItem, CommonModule]
+  imports: [IonCardContent, IonCard, IonIcon, IonItem, CommonModule, CachedImgComponent]
 })
 export class LinkComponent {
 
@@ -34,14 +35,21 @@ export class LinkComponent {
     return 'link-outline'
   });
 
-  simple = computed(() => {
+  type = computed(() => {
     let txt: string = this.text();
-
-    return !txt.includes('<');
+    let url: string = this.url();
+    if (url.endsWith('.jpeg') || url.endsWith('.jpg') || url.endsWith('.webp') || url.endsWith('.gif') || url.endsWith('.png')) {
+      return 'image';
+    }
+    if (txt.includes('<')) {
+      return 'complex';
+    }
+    return 'simple';
   });
 
   html = computed(() => {
     let txt: string = this.text();
+
     return this.removeTagsExceptAllowed(txt, ['b', 'h1', 'h2', 'h3', 'h4', 'code', 'img', 'small', 'br', 'center', 'p', 'i', 'u', 'ul', 'li', 'ol']);
   });
 
