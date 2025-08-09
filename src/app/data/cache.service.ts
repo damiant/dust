af51b9dd44e4a3f53494631972af3881c77137f2
damiantarnawsky <damiantarnawsky@gmail.com>
2025-08-09 08:01:02 -0700

    feat: cache audio

--- src/app/data/cache.service.ts
+++ src/app/data/cache.service.ts
@@ -1,7 +1,8 @@
 import { Injectable, WritableSignal, inject } from "@angular/core";
 import { DbService } from "./db.service";
-import { getCachedImage } from "./cache-store";
+import { getCachedAudio, getCachedImage } from "./cache-store";
 import { Art, Camp } from "./models";
+type CacheType = 'Image' | 'Audio';
 
 @Injectable({
     providedIn: 'root',
@@ -9,22 +10,39 @@ import { Art, Camp } from "./models";
 export class CacheService {
     private db = inject(DbService);
 
+
     public async download(status: WritableSignal<string>): Promise<string> {
         // Get all art from the database
         const arts = await this.db.findArts(undefined, undefined);
         const imagesUrls = this.getArtImages(arts);
+
+        // Get all audio
+        const audioUrls = await this.getAudio(arts);
+        await this.cacheFiles(audioUrls, status, 'Audio');
 
         // Get all camps and append their images
         const camps = await this.db.findCamps('', undefined);
         this.getCampImages(camps, imagesUrls);
 
         const currentTime = Date.now();
-        await this.cacheImages(imagesUrls, status);
+        await this.cacheFiles(imagesUrls, status, 'Image');
         if (currentTime - Date.now() >= 1000) {
             return `Images are now available offline.`;
         }
         return '';
     }
+
+    private async getAudio(arts: Art[]): Promise<string[]> {
+        const audioUrls: string[] = [];
+        // Iterate over each art piece
+        for (const art of arts) {
+            if (art.audio && art.audio.length > 0) {
+                audioUrls.push(art.audio);
+            }
+        }
+        return audioUrls;
+    }
+
 
     private getArtImages(arts: Art[]): string[] {
         const imagesUrls: string[] = [];
@@ -54,23 +72,28 @@ export class CacheService {
         }
     }
 
-    private async cacheImages(imageUrls: string[], status: WritableSignal<string>): Promise<void> {
+
+    private async cacheFiles(urls: string[], status: WritableSignal<string>, type: CacheType): Promise<void> {
         let id = 1;
         let lastUpdateTime = 0;
-        
-        for (const imageUrl of imageUrls) {
+
+        for (const url of urls) {
             try {
                 const currentTime = Date.now();
                 if (currentTime - lastUpdateTime >= 500) {
-                    status.set(`${id} of ${imageUrls.length} images downloaded`);
+                    status.set(`${id} of ${urls.length} ${type}s downloaded`);
                     lastUpdateTime = currentTime;
                 }
-                
+
                 // Attempt to cache each image
-                await getCachedImage(imageUrl);
+                if (type === 'Audio') {
+                    await getCachedAudio(url);
+                } else if (type === 'Image') {
+                    await getCachedImage(url);
+                }
                 id++;
             } catch (error) {
-                console.error(`Failed to cache image: ${imageUrl}`, error);
+                console.error(`Failed to cache ${type}: ${url}`, error);
                 id++;
             }
         }
