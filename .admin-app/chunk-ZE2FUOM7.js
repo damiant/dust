0bbfe07afa3fa70cc6a3b62d8185e7e371ea20b1
damiantarnawsky <damiantarnawsky@gmail.com>
2025-11-15 10:08:00 -0800

    fix: spm build for github actions

---
+++ .admin-app/chunk-ZE2FUOM7.js
@@ -0,0 +1,160 @@
+import{a as L}from"./chunk-DB5TGSUT.js";import"./chunk-QTBNLBDF.js";import{a as ve}from"./chunk-BPJKYUB7.js";import"./chunk-MTPZGFQA.js";import{a as he}from"./chunk-TRRH6YG6.js";import"./chunk-OZ2IKH4J.js";import"./chunk-NBGQYW5Y.js";import"./chunk-HTUMMCB5.js";import"./chunk-I4H34LCA.js";import"./chunk-GDAPN2EF.js";import"./chunk-HC6MZPB3.js";import"./chunk-YMI2B6JE.js";import"./chunk-STA5XHHU.js";import"./chunk-NPRM7GGH.js";import"./chunk-SFJBRZ2Y.js";import"./chunk-I5V6RRU4.js";import"./chunk-2EOHHFOF.js";import"./chunk-JXIEZMHN.js";import"./chunk-3QBWF6TQ.js";import"./chunk-MCRJI3T3.js";import"./chunk-GTAO53MO.js";import"./chunk-JEOCIQEZ.js";import"./chunk-MM5QLNJM.js";import"./chunk-3573TPBU.js";import{T as ue,Z as _e,a as fe}from"./chunk-2G3SQA7K.js";import{H as T,e as ge}from"./chunk-5TGYAYRP.js";import"./chunk-XMQYHBKU.js";import{$ as f,Aa as w,Cc as ce,D as M,G as l,I as U,Ia as q,Ja as Y,L as P,La as H,Lb as ne,Na as V,Ra as J,Sb as ae,Tb as oe,V as _,Va as K,W as v,Y as z,Z as I,_ as b,_b as re,aa as n,ba as a,ca as u,db as $,ea as B,ec as le,fa as S,ga as d,ib as ee,ka as Z,la as Q,lb as te,ma as X,mc as se,p as O,pc as me,qc as pe,s as c,sb as ie,t as g,ta as r,ua as h,uc as de,va as E,ya as y,za as C}from"./chunk-LVKFIXIJ.js";import"./chunk-JORYF43P.js";import"./chunk-7KGURMOZ.js";import"./chunk-R7LHOHUY.js";import"./chunk-HUOLUIW7.js";import"./chunk-CCK7OWI7.js";import"./chunk-LIBYTRNP.js";import"./chunk-3MK77LPM.js";import"./chunk-4U6PRYVA.js";import"./chunk-CZ4AHVKD.js";import"./chunk-Z4V4M3ZT.js";import"./chunk-QPVVTFFW.js";import"./chunk-J6ICYO4L.js";import"./chunk-LF5XB4YN.js";import{h as x}from"./chunk-LNJ3S2LQ.js";function ye(o,i){return`
+<style>
+@font-face {
+    font-family: 'Gelion Regular';
+    font-style: normal;
+    font-weight: normal;
+    src: local('Gelion Regular'), url('/assets/fonts/gelion-regular.woff') format('woff');
+}
+@font-face {
+    font-family: 'Grotesk Bold';
+    font-style: normal;
+    font-weight: normal;
+    src: local('Grotesk Bold'), url('/assets/fonts/grotesk-bold.woff') format('woff');
+}
+
+@font-face {
+    font-family: 'Nunito';
+    font-style: normal;
+    font-weight: normal;
+    src: local('Nunito'), url('/assets/fonts/nunito-extrabold.woff') format('woff');
+}
+
+
+p, span {
+    font-family: 'Gelion Regular';
+    margin: 0;
+    font-size: 0.8rem;
+}
+
+a {
+    text-decoration: none;
+}
+
+.desc {
+    display: ${i>0?"-webkit-box":"none"};
+    -webkit-line-clamp: ${i};
+    line-clamp: ${i};
+    -webkit-box-orient: vertical; 
+    overflow: hidden;
+    font-size: 0.7rem;
+}
+
+.time {
+    padding-top: 0rem;    
+    font-size: 0.8rem;
+}
+
+
+.event-header {
+    display: flex;
+    align-items: center;
+    padding-bottom: 0.5rem;
+}
+
+.camp-header {
+    display: flex;    
+    width: 100%;
+    justify-content: space-between;
+    align-items: flex-end;
+    padding-bottom: 0.5rem;
+}
+.title {
+    page-break-before: always;
+    margin-top: 50px;
+}
+
+.break {
+      page-break-after: always;
+}
+.after-break {
+    height: 50px;
+    border-bottom: 1px solid grey;
+}
+
+.camp {
+    color: #F61067; 
+}
+
+.event {
+    padding-top: 0.5rem;
+    padding-bottom: 0.5rem;
+    border-bottom: 1px solid rgba(0,0,0,0.1);
+    page-break-inside: avoid;
+}
+
+span {
+    margin-top: 1rem;
+    font-weight: bold;
+}
+
+h1, h2 {
+    font-family: 'Nunito';
+    margin: 0;
+}
+
+header, footer {
+    text-align: center;
+}
+
+h3 {
+    font-family: 'Grotesk Bold';    
+    font-size: 0.8rem;
+    margin: 0;
+}
+
+img {
+    height: 50px;
+    width: 50px;
+    object-fit: cover;
+    margin-right: 0.5rem;
+    border-radius: 0.5rem;
+}
+
+img.camp {
+    float: left;
+}
+
+.container {
+   max-width: 1200px;
+   margin-left: 0rem;
+   margin-right: 0rem;
+}
+
+.columns-container {
+    columns: ${o>4?"110":"150"}px ${o};
+    column-gap: 1.5rem;
+    column-rule 1px solid #f1f1f1;
+    padding: 1rem;
+
+    > * + * {
+        margin-top: 1rem;
+    }
+}
+
+@page {
+    margin-top: 1cm;
+    margin-bottom: 1cm;
+    @top-center {
+        content: element(pageHeader);
+    }
+
+    @bottom-center {
+        content: element(pageFooter);
+    }
+}
+
+.page-break {
+    page-break-before: always;
+}
+
+#pageHeader{
+    position: running(pageHeader);
+}
+
+#pageFooter{
+    position: running(pageFooter);
+}
+</style>
+`}var xe=(o,i)=>i.id;function we(o,i){if(o&1&&(n(0,"h2",0),r(1),a()),o&2){let e=d().$implicit;l(),h(e.heading)}}function Ie(o,i){if(o&1&&u(0,"img",3),o&2){let e=d().$implicit;f("src",e.image,M)}}function be(o,i){if(o&1&&(_(0,we,2,1,"h2",0),n(1,"div",1)(2,"div",2),_(3,Ie,1,1,"img",3),n(4,"div")(5,"h3"),r(6),a(),n(7,"p",4),r(8),a(),n(9,"a",5)(10,"p",6),r(11),a()()()(),n(12,"p",7),r(13),a()()),o&2){let e=i.$implicit,t=d();v(e.heading?0:-1),l(3),v(e.image&&!t.hideImages()?3:-1),l(3),h(e.title),l(2),E(" ",e.timeString," "),l(),f("href",e.campUrl,M),l(2),h(t.eventLocation(e)),l(2),h(e.description)}}var k=class k{constructor(){this.events=[];this.day=H("day");this.excluded=H("");this.hideImages=V(!1);this.pbOnDay=V(!0);this.inited=!1;this.done=q(()=>console.log(this.hideImages()));this.eventService=O(L);this.api=O(T);Y(()=>{let i=this.excluded();this.ngOnInit()})}ngOnInit(){return x(this,null,function*(){this.inited||(this.inited=!0,this.events=yield this.eventService.getPrintableEvents(this.api.vanity(),yield this.api.events({cached:!0}),this.day(),this.excluded()))})}eventLocation(i){var e,t;return ge(i.camp)?(e=i.other_location)!=null?e:"":(t=i.camp)!=null?t:""}};k.\u0275fac=function(e){return new(e||k)},k.\u0275cmp=P({type:k,selectors:[["app-print-events"]],inputs:{day:[1,"day"],excluded:[1,"excluded"],hideImages:[1,"hideImages"],pbOnDay:[1,"pbOnDay"]},outputs:{hideImages:"hideImagesChange",pbOnDay:"pbOnDayChange"},decls:2,vars:0,consts:[[1,"title"],[1,"event"],[1,"event-header"],[3,"src"],[1,"time"],[3,"href"],[1,"camp"],[1,"desc"]],template:function(e,t){e&1&&I(0,be,14,7,null,null,xe),e&2&&b(t.events)},encapsulation:2});var A=k;var Se=(o,i)=>i.id;function Ee(o,i){if(o&1&&u(0,"img",2),o&2){let e=d().$implicit;f("src",e.imageUrl,M)}}function Te(o,i){if(o&1&&(n(0,"div",0)(1,"div",1)(2,"h3"),r(3),a()(),n(4,"p"),_(5,Ee,1,1,"img",2),r(6),a()()),o&2){let e=i.$implicit,t=d();l(3),h(e.name),l(2),v(e.imageUrl&&!t.hideImages()?5:-1),l(),E(" ",e.description," ")}}var D=class D{constructor(i){this.api=i;this.camps=[];this.hideImages=V(!1)}ngOnInit(){return x(this,null,function*(){this.camps=yield this.api.camps({cached:!1}),this.camps.map(i=>{i.imageUrl&&(i.imageUrl=this.api.imageURL(i.imageUrl))})})}};D.\u0275fac=function(e){return new(e||D)(U(T))},D.\u0275cmp=P({type:D,selectors:[["app-print-camps"]],inputs:{hideImages:[1,"hideImages"]},outputs:{hideImages:"hideImagesChange"},decls:2,vars:0,consts:[[1,"event"],[1,"camp-header"],[1,"camp",3,"src"]],template:function(e,t){e&1&&I(0,Te,7,3,"div",0,Se),e&2&&b(t.camps)},encapsulation:2});var N=D;var Me=(o,i)=>i.id;function Ve(o,i){if(o&1&&u(0,"img",2),o&2){let e=d().$implicit;f("src",e.imageUrl,M)}}function Oe(o,i){if(o&1&&(n(0,"div",0)(1,"div",1)(2,"h3"),r(3),a()(),n(4,"p"),_(5,Ve,1,1,"img",2),r(6),a()()),o&2){let e=i.$implicit,t=d();l(3),h(e.name),l(2),v(e.imageUrl&&!t.hideImages()?5:-1),l(),E(" ",e.description," ")}}var F=class F{constructor(i){this.api=i;this.art=[];this.hideImages=V(!1)}ngOnInit(){return x(this,null,function*(){this.art=yield this.api.art({cached:!1}),this.art.map(i=>{i.imageUrl&&(i.imageUrl=this.api.imageURL(i.imageUrl))})})}};F.\u0275fac=function(e){return new(e||F)(U(T))},F.\u0275cmp=P({type:F,selectors:[["app-print-art"]],inputs:{hideImages:[1,"hideImages"]},outputs:{hideImages:"hideImagesChange"},decls:2,vars:0,consts:[[1,"event"],[1,"camp-header"],[1,"camp",3,"src"]],template:function(e,t){e&1&&I(0,Oe,7,3,"div",0,Me),e&2&&b(t.art)},encapsulation:2});var R=F;var ke=["frame"];function De(o,i){if(o&1&&(n(0,"ion-select-option",23),r(1),a()),o&2){let e=i.$implicit;f("value",e),l(),h(e)}}function Fe(o,i){o&1&&u(0,"app-spinner")}function We(o,i){if(o&1){let e=B();n(0,"header",28)(1,"h1"),r(2),a(),n(3,"h2"),r(4),a()(),n(5,"div",29)(6,"div",30)(7,"app-print-events",31),w("pbOnDayChange",function(s){c(e);let m=d(2);return C(m.pbOnDay,s)||(m.pbOnDay=s),g(s)})("hideImagesChange",function(s){c(e);let m=d(2);return C(m.hideImages,s)||(m.hideImages=s),g(s)}),a()()()}if(o&2){let e=i.$implicit,t=d(2);f("ngClass",t.pbOnDay?"page-break":""),l(2),h(t.title),l(2),E("",e," Events"),l(3),y("pbOnDay",t.pbOnDay),f("excluded",t.excluded),y("hideImages",t.hideImages),f("day",e)}}function Be(o,i){if(o&1&&I(0,We,8,7,null,null,z),o&2){let e=d();b(e.days)}}function Ue(o,i){if(o&1){let e=B();n(0,"header",32)(1,"h1"),r(2),a(),n(3,"h2"),r(4,"Theme Camps"),a()(),n(5,"div",29)(6,"div",30)(7,"app-print-camps",33),w("hideImagesChange",function(s){c(e);let m=d();return C(m.hideImages,s)||(m.hideImages=s),g(s)}),a()()()}if(o&2){let e=d();l(2),h(e.title),l(5),y("hideImages",e.hideImages)}}function Le(o,i){if(o&1){let e=B();n(0,"header",32)(1,"h1"),r(2),a(),n(3,"h2"),r(4,"Art"),a()(),n(5,"div",29)(6,"div",30)(7,"app-print-art",33),w("hideImagesChange",function(s){c(e);let m=d();return C(m.hideImages,s)||(m.hideImages=s),g(s)}),a()()()}if(o&2){let e=d();l(2),h(e.title),l(5),y("hideImages",e.hideImages)}}var W=class W{constructor(){this.title="";this.busy=!0;this.diffTimeZone="";this.showing="all";this.imageUrl="";this.pbOnDay=!0;this.hideImages=!1;this.filter="all";this.excludedTypes=[];this.excluded="";this.eventTypes=[];this.columns="3";this.lines="6";this.days=[];this.eventService=O(L);this.api=O(T);_e({add:fe,printOutline:ue})}ngOnInit(){return x(this,null,function*(){var e;this.title=this.api.festivalTitle(),this.diffTimeZone=this.api.currentTimeZone()==this.api.festivalTimeZone()?"":this.api.festivalTimeZone(),yield this.api.setFestivalByVanity(this.vanity),this.imageUrl=(e=this.api.imageURL(this.api.festivalImage()))!=null?e:"";let i=yield this.api.getFestival(this.api.festivalId,{cached:!0});this.eventTypes=i.event_types?i.event_types.split(`
+`):ve})}print(){this.iFrame.nativeElement.contentWindow.focus(),this.iFrame.nativeElement.contentWindow.print()}ionViewDidEnter(){return x(this,null,function*(){yield this.refresh(),setTimeout(()=>{this.busy=!1,this.copyToIFrame()},4e3)})}hasEvents(){return this.filter=="all"||this.filter=="events"}hasCamps(){return this.filter=="all"||this.filter=="camps"}hasArt(){return this.filter=="all"||this.filter=="art"}update(){this.excluded=this.excludedTypes.join(","),this.busy=!0,setTimeout(()=>{this.busy=!1,this.copyToIFrame()},4e3)}copyToIFrame(){let i=document.getElementById("page"),e=this.iFrame.nativeElement.contentDocument;e.open(),e.write(ye(parseInt(this.columns),parseInt(this.lines))),e.write(i==null?void 0:i.innerHTML),e.close()}festivalImage(){return this.api.imageURL(this.api.festivalImage())}refresh(){return x(this,null,function*(){this.days=yield this.eventService.getEventDays(this.api.vanity(),yield this.api.events({cached:!1}))})}};W.\u0275fac=function(e){return new(e||W)},W.\u0275cmp=P({type:W,selectors:[["app-print"]],viewQuery:function(e,t){if(e&1&&Z(ke,5),e&2){let s;Q(s=X())&&(t.iFrame=s.first)}},inputs:{vanity:"vanity"},decls:71,vars:18,consts:[["frame",""],["color","primary"],["slot","start"],["slot","end"],[3,"click"],["labelPlacement","end",3,"ionChange","ngModelChange","ngModel"],[2,"padding","1rem"],["slot","end","label","COLUMNS","aria-label","Columns","interface","popover",3,"ionChange","ngModelChange","ngModel","value"],["value","1"],["value","2"],["value","3"],["value","4"],["value","5"],["slot","end","label","MAX LINES","aria-label","Maximum Lines","interface","popover",3,"ionChange","ngModelChange","ngModel","value"],["value","0"],["value","6"],["value","10"],["slot","end","label","FILTER","aria-label","Filter by Type","interface","popover",3,"ionChange","ngModelChange","ngModel","value"],["value","all"],["value","camps"],["value","events"],["value","art"],["label","EXCLUDE TYPES","interface","popover","placeholder","Select event types to exclude",2,"max-width","200px",3,"ionChange","ngModelChange","multiple","value","ngModel"],[3,"value"],[3,"fullscreen"],[3,"scrolling","frameBorder"],["id","page",2,"display","none"],["id","pageFooter"],["id","pageHeader",3,"ngClass"],[1,"container"],[1,"columns-container"],[3,"pbOnDayChange","hideImagesChange","pbOnDay","excluded","hideImages","day"],["id","pageHeader",1,"page-break"],[3,"hideImagesChange","hideImages"]],template:function(e,t){if(e&1){let s=B();n(0,"ion-header")(1,"ion-toolbar",1)(2,"ion-buttons",2),u(3,"ion-back-button"),a(),n(4,"ion-title"),r(5,"Print Preview"),a(),n(6,"ion-buttons",3)(7,"ion-button",4),S("click",function(){return c(s),g(t.print())}),r(8,"Print"),a()()(),n(9,"ion-toolbar",1)(10,"ion-buttons")(11,"ion-button")(12,"ion-checkbox",5),S("ionChange",function(){return c(s),g(t.update())}),w("ngModelChange",function(p){return c(s),C(t.hideImages,p)||(t.hideImages=p),g(p)}),r(13,"Hide Images"),a()(),u(14,"div",6),n(15,"ion-select",7),S("ionChange",function(){return c(s),g(t.update())}),w("ngModelChange",function(p){return c(s),C(t.columns,p)||(t.columns=p),g(p)}),n(16,"ion-select-option",8),r(17,"1"),a(),n(18,"ion-select-option",9),r(19,"2"),a(),n(20,"ion-select-option",10),r(21,"3"),a(),n(22,"ion-select-option",11),r(23,"4"),a(),n(24,"ion-select-option",12),r(25,"5"),a()(),u(26,"div",6),n(27,"ion-select",13),S("ionChange",function(){return c(s),g(t.update())}),w("ngModelChange",function(p){return c(s),C(t.lines,p)||(t.lines=p),g(p)}),n(28,"ion-select-option",14),r(29,"0"),a(),n(30,"ion-select-option",8),r(31,"1"),a(),n(32,"ion-select-option",9),r(33,"2"),a(),n(34,"ion-select-option",10),r(35,"3"),a(),n(36,"ion-select-option",11),r(37,"4"),a(),n(38,"ion-select-option",12),r(39,"5"),a(),n(40,"ion-select-option",15),r(41,"6"),a(),n(42,"ion-select-option",16),r(43,"10"),a()(),u(44,"div",6),n(45,"ion-select",17),S("ionChange",function(){return c(s),g(t.update())}),w("ngModelChange",function(p){return c(s),C(t.filter,p)||(t.filter=p),g(p)}),n(46,"ion-select-option",18),r(47,"All"),a(),n(48,"ion-select-option",19),r(49,"Camps"),a(),n(50,"ion-select-option",20),r(51,"Events"),a(),n(52,"ion-select-option",21),r(53,"Art"),a()(),u(54,"div",6),n(55,"ion-select",22),S("ionChange",function(){return c(s),g(t.update())}),w("ngModelChange",function(p){return c(s),C(t.excludedTypes,p)||(t.excludedTypes=p),g(p)}),I(56,De,2,2,"ion-select-option",23,z),a(),n(58,"ion-button")(59,"ion-checkbox",5),S("ionChange",function(){return c(s),g(t.update())}),w("ngModelChange",function(p){return c(s),C(t.pbOnDay,p)||(t.pbOnDay=p),g(p)}),r(60,"Page Break on Day"),a()()()()(),n(61,"ion-content",24),_(62,Fe,1,0,"app-spinner"),u(63,"iframe",25,0),n(65,"div",26),_(66,Be,2,0),_(67,Ue,8,2),_(68,Le,8,2),n(69,"footer",27),r(70,"-"),a()()()}e&2&&(l(12),y("ngModel",t.hideImages),l(3),y("ngModel",t.columns),f("value",t.columns),l(12),y("ngModel",t.lines),f("value",t.lines),l(18),y("ngModel",t.filter),f("value",t.filter),l(10),f("multiple",!0)("value",t.excludedTypes),y("ngModel",t.excludedTypes),l(),b(t.eventTypes),l(3),y("ngModel",t.pbOnDay),l(2),f("fullscreen",!0),l(),v(t.busy?62:-1),l(),f("scrolling","no")("frameBorder",0),l(3),v(t.hasEvents()?66:-1),l(),v(t.hasCamps()?67:-1),l(),v(t.hasArt()?68:-1))},dependencies:[de,he,K,J,ie,ee,te,$,le,pe,oe,ne,me,ae,re,A,N,R,ce,se],styles:["iframe[_ngcontent-%COMP%]{height:100%;width:100%}ion-checkbox[_ngcontent-%COMP%]{font-family:Gelion Regular;font-size:medium}ion-select[_ngcontent-%COMP%]{font-size:medium}ion-select[_ngcontent-%COMP%]::part(icon){color:#fff!important}"]});var Ce=W;export{Ce as PrintPage};
