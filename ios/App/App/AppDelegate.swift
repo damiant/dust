ed8b650a88219af398f6d49e7fd6d41a9aff14e1
damiantarnawsky <damiantarnawsky@gmail.com>
2025-11-15 09:59:27 -0800

    chore: update project configuration and add Package.resolved for dependencies

--- ios/App/App/AppDelegate.swift
+++ ios/App/App/AppDelegate.swift
@@ -1,7 +1,7 @@
 import UIKit
 import Capacitor
-import FirebaseCore
-import FirebaseMessaging
+//import FirebaseCore
+//import FirebaseMessaging
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -10,20 +10,20 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        FirebaseApp.configure()
+        //FirebaseApp.configure()
         return true
     }
 
     func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
-    //    NotificationCenter.default.post(name: .capacitorDidRegisterForRemoteNotifications, object: deviceToken)
-      Messaging.messaging().apnsToken = deviceToken
-        Messaging.messaging().token(completion: { (token, error) in
-          if let error = error {
-              NotificationCenter.default.post(name: .capacitorDidFailToRegisterForRemoteNotifications, object: error)
-          } else if let token = token {
-              NotificationCenter.default.post(name: .capacitorDidRegisterForRemoteNotifications, object: token)
-          }
-        })
+        NotificationCenter.default.post(name: .capacitorDidRegisterForRemoteNotifications, object: deviceToken)
+//      Messaging.messaging().apnsToken = deviceToken
+//        Messaging.messaging().token(completion: { (token, error) in
+//          if let error = error {
+//              NotificationCenter.default.post(name: .capacitorDidFailToRegisterForRemoteNotifications, object: error)
+//          } else if let token = token {
+//              NotificationCenter.default.post(name: .capacitorDidRegisterForRemoteNotifications, object: token)
+//          }
+//        })
     }
 
     func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
